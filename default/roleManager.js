0986ed56c20e9a6d12f6a5d28492a129db13d144
HOME_PC\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-04-08 13:36:47 -0400

    Got things working in Visual Studio 2017 Began working on some base classes for creeps

---
+++ default/roleManager.js
@@ -0,0 +1,43 @@
+/*
+ * Manages creeps.
+ * Use: https://github.com/Garethp/Screeps/blob/master/roleManager.js
+ */
+
+module.exports = {
+roleExists: function(role){
+		try
+		{
+			require("roles_" + role);
+			return true;
+		}
+		catch(e)
+		{
+			return false;
+		}
+	},
+
+	getRole: function(role)
+	{
+		if(!this.roleExists(role))
+			return false;
+
+        var proto = require('role_prototype');
+
+		var roleObject = require("roles_" + role);
+        roleObject = require('extend')(roleObject, proto);
+		return roleObject;
+	},
+
+	getRoleBodyParts: function(role)
+	{
+		if(!this.roleExists(role))
+			return false;
+
+		var role = this.getRole(role);
+
+		if(role.getParts !== undefined)
+			return role.getParts.call(role);
+		else
+			return role.prototype.getParts.call(role);
+	}
+};
